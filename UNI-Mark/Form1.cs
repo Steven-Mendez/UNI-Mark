5887872513336df3b4aeadbb63fbe768a17d943b
Steveb Mendez <Stevenampaiz@gmail.com>
2023-04-01 17:05:18 -0600

    CPU Service

--- UNI-Mark/Form1.cs
+++ UNI-Mark/Form1.cs
@@ -1,3 +1,5 @@
+using UNI_Mark.Services;
+
 namespace UNI_Mark
 {
     public partial class Form1 : Form
@@ -5,6 +7,11 @@ namespace UNI_Mark
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CPUService cpu = new();
         }
     }
 }
---
+++ UNI-Mark/Services/CPUService.cs
@@ -0,0 +1,54 @@
+﻿using System.Management;
+
+namespace UNI_Mark.Services
+{
+    public class CPUService
+    {
+        public string? OSVersion { get; }
+        public string? Version { get; }
+        public string? ProcessorArchitecture { get; }
+        public string? ProcessorName { get; }
+        public string? ProcessorMaxClockSpeed { get; }
+        public string? ComputerSystemTotalPhysicalMemory { get; }
+        public string? LogicalDiskDescription { get; }
+        public string? LogicalDiskFreeSpace { get; }
+        public string? PhysicalMemoryCapacity { get; }
+        public string? PhysicalMemorySpeed { get; }
+
+        public CPUService()
+        {
+            ManagementObjectSearcher searcher;
+            OSVersion = Environment.OSVersion.ToString();
+            Version = Environment.Version.ToString();
+            ProcessorArchitecture = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")!;
+
+            searcher = new("SELECT * FROM Win32_Processor");
+            foreach (var queryObj in searcher.Get().Cast<ManagementObject>())
+            {
+                ProcessorName = queryObj["Name"].ToString();
+                ProcessorMaxClockSpeed = queryObj["MaxClockSpeed"].ToString();
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            foreach (var queryObj in searcher.Get().Cast<ManagementObject>())
+            {
+                ComputerSystemTotalPhysicalMemory = queryObj["TotalPhysicalMemory"].ToString();
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk WHERE DeviceID='C:'");
+            foreach (var queryObj in searcher.Get().Cast<ManagementObject>())
+            {
+                LogicalDiskDescription = queryObj["Description"].ToString();
+                LogicalDiskFreeSpace = queryObj["FreeSpace"].ToString();
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            foreach (var obj in searcher.Get().Cast<ManagementObject>())
+            {
+                PhysicalMemoryCapacity = Math.Round(Convert.ToDouble(obj["Capacity"]) / (1024 * 1024 * 1024), 2).ToString();
+                PhysicalMemorySpeed = obj["Speed"].ToString();
+            }
+
+        }
+    }
+}
